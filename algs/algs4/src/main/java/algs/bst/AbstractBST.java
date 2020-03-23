e879a3e17531d8b3ff7ea3cc6b10cbaf485f6321
agui <agui93@foxmail.com>
2020-03-23 19:13:35 +0800

    update

---
+++ algs/algs4/src/main/java/algs/bst/AbstractBST.java
@@ -0,0 +1,10 @@
+package algs.bst;
+
+/**
+ * @author agui93
+ * @since 2020/3/23
+ */
+abstract public class AbstractBST<Key extends Comparable<Key>, Value> implements BinarySearchTreeInterface<Key, Value> {
+
+
+}
