d6dcca436ee808a526d57ec26fe8645e260feef3
agui <agui93@foxmail.com>
2020-07-12 14:41:40 +0800

    update

--- algs/algs-sword/src/main/java/com/algs/sword/PrintListInReversedOrder.java
+++ algs/algs-sword/src/main/java/com/algs/sword/PrintListInReversedOrder.java
@@ -1,5 +1,7 @@
 package com.algs.sword;
 
+import java.util.Arrays;
+
 /**
  * // 面试题6：从尾到头打印链表
  * // 题目：输入一个链表的头结点，从尾到头反过来打印出每个结点的值。
@@ -9,5 +11,61 @@ package com.algs.sword;
  */
 public class PrintListInReversedOrder {
 
+    public static class ListNode {
+        private int value;
+        private ListNode next;
 
+        public ListNode(int v) {
+            this.value = v;
+        }
+    }
+
+
+    //todo by Deque Stack的JDK源码(线程非安全和线程安全的不同实现)
+    public static int[] reversePrint(ListNode head) {
+        if (head == null) {
+            return new int[0];
+        }
+
+//        java.util.Deque<ListNode> deque = new java.util.ArrayDeque<>();
+        java.util.Stack<ListNode> stack = new java.util.Stack<>();
+        ListNode node = head;
+        while (node != null) {
+            stack.push(node);
+            node = node.next;
+        }
+
+        int[] values = new int[stack.size()];
+        int i = 0;
+
+        while (!stack.isEmpty()) {
+            values[i++] = stack.pop().value;
+        }
+
+
+        return values;
+    }
+
+
+    //创建链表,返回链表head
+    public static ListNode createList(int[] values) {
+        ListNode head = null, tail = null;
+        for (int value : values) {
+            if (head == null) {
+                head = tail = new ListNode(value);
+            } else {
+                tail.next = new ListNode(value);
+                tail = tail.next;
+            }
+        }
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+        int[] values = {2, 3, 1};
+        ListNode head = createList(values);
+        int[] result = reversePrint(head);
+        System.out.println(Arrays.toString(result));
+    }
 }
